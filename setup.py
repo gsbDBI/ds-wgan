01f6939c1f05f8d5490914ce206818e9e62ba8aa
halflearned <halflearned@gmail.com>
2019-10-16 13:21:15 -0700

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,44 @@
+from os.path import abspath, dirname, join
+
+from setuptools import setup, find_packages
+
+here = abspath(dirname(__file__))
+
+with open(join(here, 'README.md')) as f:
+    readme = f.read()
+
+# Uncomment once LICENSE is added
+# with open(join(here, 'LICENSE')) as f:
+#     lic = f.read()
+
+setup(name='wgan',
+      version='0.1',
+      description='wgan',
+      author='Jonas Metzger and Evan Munro',
+      long_description=readme,
+      long_description_content_type='text/markdown',
+      url='https://github.com/gsbDBI/ds-wgan',
+      py_modules=['wgan'],
+      install_requires=[
+          "numpy",
+          "torch>=1.1.0",
+          "tqdm",
+          "tensorboardX>=1.8",
+          "tensorboard>=2.0.0",
+          "pandas",
+          "matplotlib",
+          "scipy",
+          "hypergrad @ git+ssh://git@github.com/gbaydin/hypergradient-descent.git",
+          "pyarrow>=0.14",
+      ],
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Science/Research',
+          'Topic :: Scientific/Engineering',
+          # Uncomment once license it added
+          # 'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3'
+      ],
+      # license=lic,
+      # Uncomment when license is approved
+      )
