35f92b6c27a231b747abf156be3046b296858669
Evan Munro <evan.munro@gmail.com>
2020-05-15 15:14:03 -0700

    make package installable via pip

--- setup.py
+++ setup.py
@@ -7,33 +7,30 @@ here = abspath(dirname(__file__))
 with open(join(here, 'README.md')) as f:
     readme = f.read()
 
-# Uncomment once LICENSE is added
-# with open(join(here, 'LICENSE')) as f:
-#     lic = f.read()
+with open(join(here, 'LICENSE')) as f:
+    lic = f.read()
 
 setup(name='wgan',
       version='0.2',
       description='wgan',
       author='Jonas Metzger and Evan Munro',
+      author_email='munro@stanford.edu',
       long_description=readme,
       long_description_content_type='text/markdown',
       url='https://github.com/gsbDBI/ds-wgan',
-      py_modules=['wgan'],
+      packages=find_packages(),
       install_requires=[
           "numpy",
           "torch>=1.1.0",
           "pandas",
-          "matplotlib",
-          "scipy"
+          "matplotlib"
       ],
       classifiers=[
           'Development Status :: 3 - Alpha',
           'Intended Audience :: Science/Research',
           'Topic :: Scientific/Engineering',
-          # Uncomment once license it added
-          # 'License :: OSI Approved :: MIT License',
+          'License :: OSI Approved :: MIT License',
           'Programming Language :: Python :: 3'
       ],
-      # license=lic,
-      # Uncomment when license is approved
+        license=lic,
       )
